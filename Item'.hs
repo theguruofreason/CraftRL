2a3d6315f6451ba4e639c7702b50fb12cb38c5e4
theguruofreason <theguruofreason@gmail.com>
2013-07-11 20:37:25 -0700

    Exploring inventory as Map

--- Item'.hs
+++ Item'.hs
@@ -1,66 +1,45 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DeriveGeneric #-}
 module Item where
-import           GHC.Generics (Generic)
 import           Control.Lens
 import           Data.List ((\\))
-import           Data.Vector (Vector,empty)
+import qualified Data.Map as Map
 
 qualityAdj :: [String]
 qualityAdj = ["", "miserable ", "poor ", "sub-par ", "average ", "good ", "great ", "excellent ", "legendary "]
 
-data Category = Material | Weapon | Armor | Tool
-  deriving (Show, Read, Eq, Ord)
-
-data ItemPropKey = Name | Quality | Weight | ValuePer | Category | Letter
-  deriving (Show, Read, Eq, Ord, Generic)
-
-data PropVal = Cat Category | Scalar Int | Nominal String
+data Category = Category { unCategory :: String }
+  deriving (Show, Eq)
 
 data Item = Item { _name         :: String
                  , _quality      :: Int
-                 , _itemLetter   :: Char
                  , _weight       :: Int
                  , _category     :: Category
-                 , _stackSize    :: Int
                  , _valuePer     :: Int
                  }
-            deriving (Read, Eq)
+            deriving (Eq)
 
 instance Show Item where
-  show (Item n q l w _ s _) = l : " - " ++ qual ++ n ++ wt ++ show s
+  show (Item n q w _ _) = qual ++ n ++ wt
                               where
                                 qual = qualityAdj !! q
                                 wt = " (" ++ show w ++ ") "
 
-anaxe = [(Category, Cat Tool),(ValuePer, Scalar 10),(Weight, Scalar 4),(Quality, Scalar 2),(Name, Nominal "axe")]
+type Inventory = Map.Map Char (Item,Int)
 
-
-type Inventory = Vector Item
-
-data Player = Player { _inventory :: Inventory }
-  deriving (Show, Read, Eq)
+data Player = Player { _inventory        :: Inventory
+                     , _armorcraft       :: Float
+                     , _weaponcraft      :: Float
+                     , _tailoring        :: Float
+                     , _materialEff      :: Float
+                     }
+  deriving (Show, Eq)
 
 data Game = Game { _player :: Player }
-
-joe = Player empty
-theaxe = Item "axe" 4 'a' 3 Tool 1 10
-asword = Item "sword" 6 'b' 4 Weapon 1 10
-someiron = Item "bar of iron" 0 'c' 2 Material 3 5
 
 $(makeLenses ''Item)
 $(makeLenses ''Player)
 
-addItem :: Player -> Item -> Player
-addItem player item = case (player^.inventory & ifind p) of
-                        Just (i,_) -> Player $ player^.inventory & ix i %~ stackItems
-                        Nothing    -> Player $ item <| player^.inventory
-    where
-      stackItems i = i & stackSize +~ item^.stackSize
-      -- availLetter = head $ (['a'..'z'] ++ ['A'..'Z']) \\ (map _itemLetter (_inventory player))
-      p _ i = i^.name    == item^.name
-           && i^.quality == item^.quality
-
-{-removeItem player item
-  | haveItem item player =
-  | otherwise = "Item not found in inventory!"-}
+addItem :: (Char,(Item,Int)) -> Player -> Player
+addItem (l,(i,n)) player = case i `elem` map (fst.snd) (Map.toList $ player^.inventory) of
+                         True -> over (player.traverse.inventory) (Map.adjust (\(x,s) -> (x,s+n)) l (player^.inventory)) player
