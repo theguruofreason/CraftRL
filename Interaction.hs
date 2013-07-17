f56a939ce0eb50187d6bb70ceafc6b6a12670b96
theguruofreason <theguruofreason@gmail.com>
2013-07-16 18:28:50 -0700

    + functions to modify recipes

--- Interaction.hs
+++ Interaction.hs
@@ -9,3 +9,9 @@ craftInvModify :: Recipe -> Player -> Player
 craftInvModify theRecipe thePlayer = (theRecipe^.produced) `addItem` (takeIngredients)
   where
     takeIngredients = foldr removeItem thePlayer $ theRecipe^.required.inventory
+
+modRecPlayerInv :: (ItemSlot -> Player -> Player) -> ItemSlot -> Recipe -> Recipe
+modRecPlayerInv f itemslot theRecipe = theRecipe & (required) %~ f itemslot
+
+modRecIngredients :: (Ingredient -> [Ingredient] -> [Ingredient]) -> Ingredient -> Recipe -> Recipe
+modRecIngredients f theIngredient theRecipe = theRecipe & (ingredients) %~ (f theIngredient)
